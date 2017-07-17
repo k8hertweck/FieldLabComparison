95d9fdcd4f845ba2726d2bdf358290bb5e187ed4
k8hertweck <k8hertweck@gmail.com>
2017-07-17 15:47:33 -0500

    correcting custom silva files

--- mothur_SilvaRef.bat
+++ mothur_SilvaRef.bat
@@ -8,16 +8,13 @@
 #or using the silva reference files from the MiSeq SOP: silva.bacteria.fasta 
 
 # aligns known sequence to complete database
-#input: E.coliV1-V3.fasta, silva.bacteria.fasta 
-#output: E.coliV1-V3.align, E.coliV1-V3.align.report, E.coliV1-V3.flip.accnos file
-align.seqs(fasta=silva/E.coliV1-V3.fasta, template=silva/silva.bacteria.fasta)
+# output: E.coliV1V3.align, E.coliV1V3.align.report, E.coliV1V3.flip.accnos file
+align.seqs(fasta=silva/E.coliV1V3.fas, template=silva/silva.bacteria.fasta)
 
 # identify start and stop of V1-V3 region of 16S rRNA 
-#input: E.coliV1-V3.align
-#output: E.coliV1-V3.summary
-summary.seqs(fasta=silva/E.coliV1-V3.align)
+# output: E.coliV1V3.summary
+summary.seqs(fasta=silva/E.coliV1V3.align)
 
 # trim the template to V1-V3
-#input: silva.bacteria.fasta
-#output: silva.bacteria.pcr.fasta 
+# output: silva.bacteria.pcr.fasta 
 pcr.seqs(fasta=silva/silva.bacteria.fasta, start=1115, end=13881, keepdots=FALSE)
