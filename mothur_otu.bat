d739a1984b98b20f47b5209f18cf3ba66a2170a6
k8hertweck <k8hertweck@gmail.com>
2017-07-18 15:27:54 -0500

    moving sequence trimming and aggregation out

--- mothur_otu.bat
+++ mothur_otu.bat
@@ -4,130 +4,65 @@
 # execute within project directory
 # for explanation of commands: https://www.mothur.org/wiki/Sequence_processing
 
-#convert fastq files fasta + qual files
-fastq.info(fastq=data/SRP018246/SRR655327.fastq)
-fastq.info(fastq=data/SRP018246/SRR655328.fastq)
-fastq.info(fastq=data/SRP018246/SRR655329.fastq)
-fastq.info(fastq=data/SRP018246/SRR655330.fastq)
-fastq.info(fastq=data/SRP018246/SRR655331.fastq)
-fastq.info(fastq=data/SRP018246/SRR655332.fastq)
-fastq.info(fastq=data/SRP018246/SRR655333.fastq)
-fastq.info(fastq=data/SRP018246/SRR655334.fastq)
-fastq.info(fastq=data/SRP018246/SRR655335.fastq)
-fastq.info(fastq=data/SRP018246/SRR655336.fastq)
-fastq.info(fastq=data/SRP018246/SRR655337.fastq)
-fastq.info(fastq=data/SRP018246/SRR655338.fastq)
-fastq.info(fastq=data/SRP018246/SRR655339.fastq)
-fastq.info(fastq=data/SRP018246/SRR655340.fastq)
-fastq.info(fastq=data/SRP018246/SRR655341.fastq)
-fastq.info(fastq=data/SRP018246/SRR655342.fastq)
-fastq.info(fastq=data/SRP018246/SRR655343.fastq)
-fastq.info(fastq=data/SRP018246/SRR655344.fastq)
-fastq.info(fastq=data/SRP018246/SRR655345.fastq)
-fastq.info(fastq=data/SRP018246/SRR655346.fastq)
-fastq.info(fastq=data/SRP018246/SRR655347.fastq)
-fastq.info(fastq=data/SRP018246/SRR655348.fastq)
-fastq.info(fastq=data/SRP018246/SRR655349.fastq)
-fastq.info(fastq=data/SRP018246/SRR655350.fastq)
-fastq.info(fastq=data/SRP018246/SRR655351.fastq)
-fastq.info(fastq=data/SRP018246/SRR655352.fastq)
-fastq.info(fastq=data/SRP018246/SRR655353.fastq)
-fastq.info(fastq=data/SRP018246/SRR655354.fastq)
-fastq.info(fastq=data/SRP018246/SRR655355.fastq)
-fastq.info(fastq=data/SRP018246/SRR655356.fastq)
-fastq.info(fastq=data/SRP018246/SRR655357.fastq)
-fastq.info(fastq=data/SRP018246/SRR655358.fastq)
-fastq.info(fastq=data/SRP018246/SRR655359.fastq)
-fastq.info(fastq=data/SRP018246/SRR655360.fastq)
-fastq.info(fastq=data/SRP018246/SRR655361.fastq)
-fastq.info(fastq=data/SRP018246/SRR655362.fastq)
-fastq.info(fastq=data/SRP018246/SRR655363.fastq)
-fastq.info(fastq=data/SRP018246/SRR655364.fastq)
-
-# move fata and qual files to analysis/
-system(mkdir analysis/)
-system(mv data/*/*.fasta data/*/*.qual analysis/)
-
-# set working directory to analysis for rest of script
+# change mothur working directory to analysis
 set.dir(input=analysis)
 
-# create group file
-# output mergegroups
-make.group(fasta=SRR655327.fasta-SRR655328.fasta-SRR655329.fasta-SRR655330.fasta-SRR655331.fasta-SRR655332.fasta-SRR655333.fasta-SRR655334.fasta-SRR655335.fasta-SRR655336.fasta-SRR655337.fasta-SRR655338.fasta-SRR655339.fasta-SRR655340.fasta-SRR655341.fasta-SRR655342.fasta-SRR655343.fasta-SRR655344.fasta-SRR655345.fasta-SRR655346.fasta-SRR655347.fasta-SRR655348.fasta-SRR655349.fasta-SRR655350.fasta-SRR655351.fasta-SRR655352.fasta-SRR655353.fasta-SRR655354.fasta-SRR655355.fasta-SRR655356.fasta-SRR655357.fasta-SRR655358.fasta-SRR655359.fasta-SRR655360.fasta-SRR655361.fasta-SRR655362.fasta-SRR655363.fasta-SRR655364.fasta, groups=field-field-field-field-lab-lab-lab-lab-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-lab-lab-lab-lab-field-field-field-field)
-
-# rename groups file
-system(cp analysis/mergegroups analysis/analysis.groups)
-#rename.file(input=mergegroups, new=analysis.groups)
-
-# make master fasta file (combines individual fasta files into a single file, analysis.fasta)
-merge.files(input=SRR655327.fasta-SRR655328.fasta-SRR655329.fasta-SRR655330.fasta-SRR655331.fasta-SRR655332.fasta-SRR655333.fasta-SRR655334.fasta-SRR655335.fasta-SRR655336.fasta-SRR655337.fasta-SRR655338.fasta-SRR655339.fasta-SRR655340.fasta-SRR655341.fasta-SRR655342.fasta-SRR655343.fasta-SRR655344.fasta-SRR655345.fasta-SRR655346.fasta-SRR655347.fasta-SRR655348.fasta-SRR655349.fasta-SRR655350.fasta-SRR655351.fasta-SRR655352.fasta-SRR655353.fasta-SRR655354.fasta-SRR655355.fasta-SRR655356.fasta-SRR655357.fasta-SRR655358.fasta-SRR655359.fasta-SRR655360.fasta-SRR655361.fasta-SRR655362.fasta-SRR655363.fasta-SRR655364.fasta, output=analysis/analysis.fasta)
-
-# make master qual file
-merge.files(input=SRR655327.qual-SRR655328.qual-SRR655329.qual-SRR655330.qual-SRR655331.qual-SRR655332.qual-SRR655333.qual-SRR655334.qual-SRR655335.qual-SRR655336.qual-SRR655337.qual-SRR655338.qual-SRR655339.qual-SRR655340.qual-SRR655341.qual-SRR655342.qual-SRR655343.qual-SRR655344.qual-SRR655345.qual-SRR655346.qual-SRR655347.qual-SRR655348.qual-SRR655349.qual-SRR655350.qual-SRR655351.qual-SRR655352.qual-SRR655353.qual-SRR655354.qual-SRR655355.qual-SRR655356.qual-SRR655357.qual-SRR655358.qual-SRR655359.qual-SRR655360.qual-SRR655361.qual-SRR655362.qual-SRR655363.qual-SRR655364.qual, output=analysis/analysis.qual)
-
 # summarize complete sequences
-summary.seqs(fasta=analysis.fasta)
-
-# trim sequences for quality
-# output files: trim.fasta, scrap.fasta, trim.qual, and scrap.qual files
-trim.seqs(fasta=analysis.fasta, qfile=analysis.qual, maxambig=1, maxhomop=8, flip=T, qaverage=25, minlength=200, processors=6)
- 
-# inspect trimmed sequences
-# output: summary file
+# output: analysis.trim.summary
 summary.seqs(fasta=analysis.trim.fasta)
 
 # simplifying the dataset to only unique sequences
-# output: names file, unique.fasta file
+# output: analysis.trim.names, analysis.trim.unique.fasta 
 unique.seqs(fasta=analysis.trim.fasta)
 
 # inspect unique sequences
-# output: unique.summary file
+# output: analysis.trim.unique.summary 
 summary.seqs(fasta=analysis.trim.unique.fasta, name=analysis.trim.names)
 
 # align our data to silva reference
-# output: unique.align, unique.align.report, unique.flip.accnos file
+# output: analysis.trim.unique.align, analysis.trim.unique.align.report, analysis.trim.unique.flip.accnos 
 align.seqs(fasta=analysis.trim.unique.fasta, reference=silva/silva.bacteria.pcr.fasta, flip=T, processors=6)
 
 # inspect aligned sequences
-# output: unique.summary
+# output: analysis.trim.unique.summary
 summary.seqs(fasta=analysis.trim.unique.align, name=analysis.trim.names)
 
 # remove sequences outside the desired range of alignment space
-# output: unique.good.align, unique.bad.accnos, good.names, good.groups file
+# output: analysis.trim.unique.good.align, analysis.trim.unique.bad.accnos, analysis.good.names, analysis.trim.good.groups file
 screen.seqs(fasta=analysis.trim.unique.align, name=analysis.trim.names, group=analysis.groups, optimize=end, criteria=90, processors=6)
 
 # inspect sequences 
-# output: unique.good.summary file
+# output: analysis.trim.unique.good.summary 
 summary.seqs(fasta=analysis.trim.unique.good.align, name=analysis.trim.good.names) 
 
 # filter alignment to remove columns that don't contain data
 # the parameter trump=. will remove any column that has a "." character, which indicates missing data
 # vertical=T option will remove any column that contains exclusively gaps
-# output: unique.good.filter.fasta, filter file
+# output: analysis.trim.unique.good.filter.fasta, filter file
 filter.seqs(fasta=analysis.trim.unique.good.align, vertical=T, processors=6)
 
 # remove redundant sequences in the alignment region
-# output: unique.good.filter.names, unique.good.filter.unique.fasta
+# output: analysis.trim.unique.good.filter.names, analysis.trim.unique.good.filter.unique.fasta
 unique.seqs(fasta=analysis.trim.unique.good.filter.fasta, name=analysis.trim.good.names)
 
 # pre-cluster within each group 
-# output: unique.good.filter.unique.precluster.fasta, unique.good.filter.unique.precluster.names, unique.good.filter.unique.precluster.field.map,and unique.good.filter.unique.precluster.lab.map file
-pre.cluster(fasta=analysis.trim.unique.good.filter.unique.subsample.fasta, name=analysis.trim.unique.good.filter.subsample.names, group=analysis.good.groups, diffs=2)
+# output: analysis.trim.unique.good.filter.unique.precluster.fasta, analysis.trim.unique.good.filter.unique.precluster.names, analysis.trim.unique.good.filter.unique.precluster.field.map, analysis.trim.unique.good.filter.unique.precluster.lab.map 
+pre.cluster(fasta=analysis.trim.unique.good.filter.unique.fasta, name=analysis.trim.unique.good.filter.names, group=analysis.good.groups, diffs=2, processors=6)
 
 # inspect the preclustered files
-# output: unique.good.filter.unique.precluster.summary file
+# output: analysis.trim.unique.good.filter.unique.precluster.summary 
 summary.seqs(fasta=analysis.trim.unique.good.filter.unique.precluster.fasta, name=analysis.trim.unique.good.filter.unique.precluster.names)
 
 # detecting and removing chimeras (unnecessary since completed in SRA data)
-# output: unique.good.filter.unique.precluster.denovo.uchime.chimeras and unique.good.filter.unique.precluster.denovo.uchime.accnos file
+# output: analysis.trim.unique.good.filter.unique.precluster.denovo.uchime.chimeras, analysis.trim.unique.good.filter.unique.precluster.denovo.uchime.accnos 
 #chimera.uchime(fasta=analysis.trim.unique.good.filter.unique.precluster.fasta, name=analysis.trim.unique.good.filter.unique.precluster.names, group=analysis.good.groups, processors=6)
 
-# rename files 
-#system(cp analysis.trim.unique.good.filter.unique.precluster.fasta final.fasta)
+# rename files (rename command is unreliable)
+system(cp analysis.trim.unique.good.filter.unique.precluster.fasta final.fasta)
 rename.file(input=analysis.trim.unique.good.filter.unique.precluster.fasta, output=final.fasta)
-#system(cp analysis.trim.unique.good.filter.unique.precluster.names final.names)
+system(cp analysis.trim.unique.good.filter.unique.precluster.names final.names)
 rename.file(input=analysis.trim.unique.good.filter.unique.precluster.names, output=final.names)
-#system(cp analysis.trim.good.groups final.groups)
+system(cp analysis.trim.good.groups final.groups)
 rename.file(input=analysis.trim.good.groups, output=final.groups)
 
 # Option 1 or 2 are different methods available to build OTUs
---
+++ mothur_prep.sh
@@ -0,0 +1,50 @@
+## Microbiome Ant Field vs Lab Analysis OTUs using mothur
+
+# usage: ./mothur_prep.sh OR bash mothur_prep.sh
+# execute within project directory
+# for explanation of commands: https://www.mothur.org/wiki/Sequence_processing
+
+cd `pwd`
+
+cd data
+
+#convert fastq files fasta + qual files
+for x in `cat SRP018246.lst`
+	do
+		mothur "#fastq.info(fastq=SRP018246/$x.fastq)"
+done
+#for x in `cat SRP018247.lst`
+#	do
+#		mothur "fastq.info(fastq=SRP018247/$x.fastq)"
+#done
+
+rm *.logfile
+
+# move fasta and qual files to analysis/
+cd ..
+mkdir analysis
+mv data/*/*.fasta data/*/*.qual analysis/
+cd analysis
+
+# trim sequences for quality
+# output files: trim.fasta, scrap.fasta, trim.qual, and scrap.qual files
+ls *.fasta | sed s/\.fasta// > all.lst
+for x in `cat all.lst`
+	do
+		mothur "#trim.seqs(fasta=$x.fasta, qfile=$x.qual, maxambig=1, maxhomop=8, flip=T, qaverage=25, minlength=200, processors=6)"
+done
+
+# create group file
+# output mergegroups
+mothur "#make.group(fasta=SRR655327.trim.fasta-SRR655328.trim.fasta-SRR655329.trim.fasta-SRR655330.trim.fasta-SRR655331.trim.fasta-SRR655332.trim.fasta-SRR655333.trim.fasta-SRR655334.trim.fasta-SRR655335.trim.fasta-SRR655336.trim.fasta-SRR655337.trim.fasta-SRR655338.trim.fasta-SRR655339.trim.fasta-SRR655340.trim.fasta-SRR655341.trim.fasta-SRR655342.trim.fasta-SRR655343.trim.fasta-SRR655344.trim.fasta-SRR655345.trim.fasta-SRR655346.trim.fasta-SRR655347.trim.fasta-SRR655348.trim.fasta-SRR655349.trim.fasta-SRR655350.trim.fasta-SRR655351.trim.fasta-SRR655352.trim.fasta-SRR655353.trim.fasta-SRR655354.trim.fasta-SRR655355.trim.fasta-SRR655356.trim.fasta-SRR655357.trim.fasta-SRR655358.trim.fasta-SRR655359.trim.fasta-SRR655360.trim.fasta-SRR655361.trim.fasta-SRR655362.trim.fasta-SRR655363.trim.fasta-SRR655364.trim.fasta, groups=field-field-field-field-lab-lab-lab-lab-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-field-lab-lab-lab-lab-field-field-field-field)"
+
+# rename groups file
+cp mergegroups analysis.groups
+
+# make master fasta file (combines individual fasta files into a single file, analysis.fasta)
+mothur "#merge.files(input=SRR655327.trim.fasta-SRR655328.trim.fasta-SRR655329.trim.fasta-SRR655330.trim.fasta-SRR655331.trim.fasta-SRR655332.trim.fasta-SRR655333.trim.fasta-SRR655334.trim.fasta-SRR655335.trim.fasta-SRR655336.trim.fasta-SRR655337.trim.fasta-SRR655338.trim.fasta-SRR655339.trim.fasta-SRR655340.trim.fasta-SRR655341.trim.fasta-SRR655342.trim.fasta-SRR655343.trim.fasta-SRR655344.trim.fasta-SRR655345.trim.fasta-SRR655346.trim.fasta-SRR655347.trim.fasta-SRR655348.trim.fasta-SRR655349.trim.fasta-SRR655350.trim.fasta-SRR655351.trim.fasta-SRR655352.trim.fasta-SRR655353.trim.fasta-SRR655354.trim.fasta-SRR655355.trim.fasta-SRR655356.trim.fasta-SRR655357.trim.fasta-SRR655358.trim.fasta-SRR655359.trim.fasta-SRR655360.trim.fasta-SRR655361.trim.fasta-SRR655362.trim.fasta-SRR655363.trim.fasta-SRR655364.trim.fasta, output=analysis.trim.fasta)"
+
+# make master qual file
+mothur "#merge.files(input=SRR655327.trim.qual-SRR655328.trim.qual-SRR655329.trim.qual-SRR655330.trim.qual-SRR655331.trim.qual-SRR655332.trim.qual-SRR655333.trim.qual-SRR655334.trim.qual-SRR655335.trim.qual-SRR655336.trim.qual-SRR655337.trim.qual-SRR655338.trim.qual-SRR655339.trim.qual-SRR655340.trim.qual-SRR655341.trim.qual-SRR655342.trim.qual-SRR655343.trim.qual-SRR655344.trim.qual-SRR655345.trim.qual-SRR655346.trim.qual-SRR655347.trim.qual-SRR655348.trim.qual-SRR655349.trim.qual-SRR655350.trim.qual-SRR655351.trim.qual-SRR655352.trim.qual-SRR655353.trim.qual-SRR655354.trim.qual-SRR655355.trim.qual-SRR655356.trim.qual-SRR655357.trim.qual-SRR655358.trim.qual-SRR655359.trim.qual-SRR655360.trim.qual-SRR655361.trim.qual-SRR655362.trim.qual-SRR655363.trim.qual-SRR655364.trim.qual, output=analysis.trim.qual)"
+
+rm *.logfile
