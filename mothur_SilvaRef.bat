377c1043a759e3fe123e101a1b2076fd8e497446
aortiz24 <aortiz1865@gmail.com>
2017-08-01 14:10:31 -0500

    modified names of output files

--- mothur_SilvaRef.bat
+++ mothur_SilvaRef.bat
@@ -8,7 +8,7 @@
 #or using the updated silva reference files from the MiSeq SOP: silva.nr_v128.align 
 
 # aligns known sequence to complete database
-# output: E.coliV1V3.align, E.coliV1V3.align.report, E.coliV1V3.flip.accnos file
+# output: E.coliV1V3.align and E.coliV1V3.align.report
 align.seqs(fasta=Silva.nr_v128/E.coliV1V3.fas, template=Silva.nr_v128/silva.nr_v128.align)
 
 # identify start and stop of V1-V3 region of 16S rRNA 
@@ -16,5 +16,5 @@ align.seqs(fasta=Silva.nr_v128/E.coliV1V3.fas, template=Silva.nr_v128/silva.nr_v
 summary.seqs(fasta=Silva.nr_v128/E.coliV1V3.align)
 
 # trim the template to V1-V3
-# output: silva.bacteria.pcr.fasta 
+# output: silva.nr_v128.pcr.align 
 pcr.seqs(fasta=Silva.nr_v128/silva.nr_v128.align, start=1115, end=13881, keepdots=FALSE)
