14a75f7dedae159f5e0c7e7a58c7a2f18aa45557
Andrea Ortiz <aortiz1865@gmail.com>
2018-02-09 13:05:27 -0600

    added automated download code

--- dataDownload.sh
+++ dataDownload.sh
@@ -5,6 +5,17 @@
 
 cd `pwd`
 
+#setting up silva reference file in silva directory
+#download silva reference file version 128
+wget https://www.mothur.org/w/images/b/b4/Silva.nr_v128.tgz
+
+#untar and gunzip silva reference file
+tar -zxvf Silva.nr_v128.tgz
+
+#move silva.nr_v128.align reference file  to silva directory
+mv silva.nr_v128.align silva/
+
+#setting up data files and directories
 mkdir data/SRP018246 data/SRP018247
 cut -f 6 data/SraRunTableSRP018246corrected.txt | tail +2 > data/SRP018246.lst
 cut -f 6 data/SraRunTableSRP018247.txt | tail +2 > data/SRP018247.lst
