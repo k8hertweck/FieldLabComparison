922988ce543d83b10a49c1f6c546599b7db73143
aortiz24 <aortiz1865@gmail.com>
2018-03-14 15:06:15 -0500

    edited code to run script within the main script

--- PairEndSeqPrep.sh
+++ PairEndSeqPrep.sh
@@ -1,8 +1,8 @@
 #Join paired end sequences for Seal data
 #R1 fastq files has forward sequences
 #R2 fastq file has reverse sequences
-join_paired_ends.py -f SAM1-16_S3_L001_R1_001.fastq -r SAM1-16_S3_L001_R2_001.fastq -o JoinResults
+join_paired_ends.sh -f SAM1-16_S3_L001_R1_001.fastq -r SAM1-16_S3_L001_R2_001.fastq -o JoinResults
 
-extract_barcodes.py -f JoinResults/joinfile -c barcode_paired_stitched --attempt_read_reorientation --bc1_len 8 --bc2_len 8 --rev_comp_bc2 -m mapping_file.txt -o barcodeResults
+extract_barcodes.sh -f JoinResults/joinfile -c barcode_paired_stitched --attempt_read_reorientation --bc1_len 8 --bc2_len 8 --rev_comp_bc2 -m mapping_file.txt -o barcodeResults
 
-split_libraries_fastq.py -i barcodeResults/fastqread -b $barcode -m mapping_file.txt -q $qual --barcode_type 8 -o $output
+split_libraries_fastq.sh -i barcodeResults/fastqread -b $barcode -m mapping_file.txt -q $qual --barcode_type 8 -o $output
