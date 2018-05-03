158d41f534af7a710ce8c79a39fa675b170edc0b
Andrea Ortiz <aortiz1865@gmail.com>
2018-05-03 18:53:22 -0500

    automating silva database download

--- mothur_SilvaRef.bat
+++ mothur_SilvaRef.bat
@@ -3,6 +3,20 @@
 # usage: mothur mothur_SilvaRef.bat
 # execute within project directory
 
+cd `pwd`
+
+cd silva
+
+#download mothur-compatible silva 128 release
+wget "https://mothur.org/w/images/b/b4/Silva.nr_v128.tgz"
+
+cp Silva.nr_v128.gz silva.nr_v128.align.gz
+
+rm Silva.nr_v128.gz
+
+#decompress mothur-compatible silva 128 release
+gunzip silva.nr_v128.align.gz
+
 # aligns known sequence to complete database
 # move silva.nr_128.align to the silva directory
 # output: E.coliV1V3.align and E.coliV1V3.align.report
