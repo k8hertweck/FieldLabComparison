fcd94db578e5d9fe4cc769d6adbe1e1a3976e465
k8hertweck <k8hertweck@gmail.com>
2018-06-04 12:12:59 -0500

    improving OTU calling
    adding comments, processors, correcting parameters

--- mothur_otu.bat
+++ mothur_otu.bat
@@ -3,6 +3,7 @@
 ## usage: mothur mothur_otu.bat
 # execute within project directory
 # for explanation of commands: https://www.mothur.org/wiki/Sequence_processing
+# any step without time estimate takes less than 1 minute
 
 # change mothur working directory to analysis
 set.dir(input=analysis)
@@ -19,7 +20,7 @@ unique.seqs(fasta=analysis.trim.fasta)
 # output: analysis.trim.unique.summary 
 summary.seqs(fasta=analysis.trim.unique.fasta, name=analysis.trim.names)
 
-# align our data to silva reference
+# align our data to silva reference (takes ~10 minutes)
 # output: analysis.trim.unique.align, analysis.trim.unique.align.report, analysis.trim.unique.flip.accnos 
 align.seqs(fasta=analysis.trim.unique.fasta, reference=silva/silva.nr_v128.pcr.align, flip=T, processors=6)
 
@@ -28,7 +29,7 @@ align.seqs(fasta=analysis.trim.unique.fasta, reference=silva/silva.nr_v128.pcr.a
 summary.seqs(fasta=analysis.trim.unique.align, name=analysis.trim.names)
 
 # remove sequences outside the desired range of alignment space
-# output: analysis.trim.unique.good.align, analysis.trim.unique.bad.accnos, analysis.good.names, analysis.trim.good.groups file
+# output: analysis.trim.unique.good.align, analysis.trim.unique.bad.accnos, analysis.trim.good.names, analysis.trim.good.groups file
 screen.seqs(fasta=analysis.trim.unique.align, name=analysis.trim.names, group=analysis.groups, optimize=end, criteria=90, processors=6)
 
 # inspect sequences 
@@ -45,7 +46,7 @@ filter.seqs(fasta=analysis.trim.unique.good.align, vertical=T, processors=6)
 # output: analysis.trim.unique.good.filter.names, analysis.trim.unique.good.filter.unique.fasta
 unique.seqs(fasta=analysis.trim.unique.good.filter.fasta, name=analysis.trim.good.names)
 
-# pre-cluster within each group 
+# pre-cluster within each group (takes hours)
 # output: analysis.trim.unique.good.filter.unique.precluster.fasta, analysis.trim.unique.good.filter.unique.precluster.names, analysis.trim.unique.good.filter.unique.precluster.field.map, analysis.trim.unique.good.filter.unique.precluster.lab.map 
 pre.cluster(fasta=analysis.trim.unique.good.filter.unique.fasta, name=analysis.trim.unique.good.filter.names, group=analysis.good.groups, diffs=2, processors=6)
 
@@ -61,25 +62,19 @@ summary.seqs(fasta=analysis.trim.unique.good.filter.unique.precluster.fasta, nam
 # output: analysis.trim.unique.good.filter.unique.precluster.count_table
 count.seqs(name=analysis.trim.unique.good.filter.unique.precluster.names)
 
-# generate taxonomy file
-# output: analysis.trim.unique.good.filter.unique.precluster.taxonomy
-classify.seqs(fasta=analysis.trim.unique.good.filter.unique.precluster.fasta, count=analysis.trim.unique.good.filter.unique.precluster.count_table, reference=silva/silva.nr_v128.pcr.align, taxonomy=silva/silva.nr_v128.tax, cutoff=80)
+# generate taxonomy file (takes ~1 hour)
+# output: analysis.trim.unique.good.filter.unique.precluster.nr_v128.wang.taxonomy
+classify.seqs(fasta=analysis.trim.unique.good.filter.unique.precluster.fasta, count=analysis.trim.unique.good.filter.unique.precluster.count_table, reference=silva/silva.nr_v128.pcr.align, taxonomy=silva/silva.nr_v128.tax, cutoff=80, processors=6)
 
 # rename files (rename command is unreliable)
 system(cp analysis/analysis.trim.unique.good.filter.unique.precluster.fasta analysis/final.fasta)
-#rename.file(input=analysis.trim.unique.good.filter.unique.precluster.fasta, output=final.fasta)
 system(cp analysis/analysis.trim.unique.good.filter.unique.precluster.names analysis/final.names)
-#rename.file(input=analysis.trim.unique.good.filter.unique.precluster.names, output=final.names)
-
 system(cp analysis/analysis.trim.unique.good.filter.unique.precluster.nr_v128.wang.taxonomy analysis/final.taxonomy)
-#rename.file(input=analysis.trim.unique.good.filter.unique.precluster.nr_v128.wang.taxonomy, output=final.taxonomy)
-
 system(cp analysis/analysis.good.groups analysis/final.groups)
-#rename.file(input=analysis.good.groups, output=final.groups)
 
 # build OTUs
 # two methods, option 1 used here
-# Option 1: generate distance matrix
+# Option 1: generate distance matrix (9 hours to calculate distance, )
 # cutoff of 0.15 means removing all pairwise distance larger than 0.15
 # If output is >100 GBs of memory, something is wrong
 # output: final.dist
@@ -96,16 +91,16 @@ cluster(column=final.dist, name=final.names)
 # output: final.an.shared file
 make.shared(list=final.an.list, group=final.groups, label=0.03)
 
+# extract taxonomy for each OTU
+classify.otu(list=final.an.list, name=final.names, taxonomy=final.taxonomy, label=0.03)
+
 # normalize by number of sequences
 # count sequences
 count.groups()
-# sub-sample to fewest sequences (4420)
+# sub-sample to fewest sequences (57316)
 # input: final.an.shared file
 # output: final.an.0.03.subsample.shared
-sub.sample(shared=final.an.shared, size=4420)
-
-# extract taxonomy for each OTU
-classify.otu(list=final.an.list, name=final.names, taxonomy=final.taxonomy, label=0.03)
+sub.sample(shared=final.an.shared, size=57316)
 
 # build phylogenetic tree
 # construct distance matrix
