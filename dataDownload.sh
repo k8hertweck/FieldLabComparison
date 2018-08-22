797258dadb1842f345db822bc3310e3aa2aa52a7
k8hertweck <k8hertweck@gmail.com>
2018-08-22 16:27:05 -0700

    updating default to Mac

--- dataDownload.sh
+++ dataDownload.sh
@@ -12,9 +12,9 @@ cd `pwd`
 # setting up silva reference file in silva directory
 # download silva reference file version 128
 # Mac
-#curl https://www.mothur.org/w/images/b/b4/Silva.nr_v128.tgz -o Silva.nr_v128.tgz
+curl https://www.mothur.org/w/images/b/b4/Silva.nr_v128.tgz -o Silva.nr_v128.tgz
 # Linux/Ubuntu
-wget https://www.mothur.org/w/images/b/b4/Silva.nr_v128.tgz
+#wget https://www.mothur.org/w/images/b/b4/Silva.nr_v128.tgz
 
 # untar and gunzip silva reference file and send to directory
 tar -zxvf Silva.nr_v128.tgz -C silva/
@@ -24,11 +24,11 @@ rm *.tgz
 # setting up data files and directories
 mkdir data/SRP018246 data/SRP018247
 # Mac
-#cut -f 6 data/SraRunTableSRP018246corrected.txt | tail +2 | sort > data/SRP018246.lst
-#cut -f 6 data/SraRunTableSRP018247.txt | tail +2 | sort > data/SRP018247.lst
+cut -f 6 data/SraRunTableSRP018246corrected.txt | tail +2 | sort > data/SRP018246.lst
+cut -f 6 data/SraRunTableSRP018247.txt | tail +2 | sort > data/SRP018247.lst
 # Linux/Ubuntu
-cut -f 6 data/SraRunTableSRP018246corrected.txt | tail --lines=+2 | sort > data/SRP018246.lst
-cut -f 6 data/SraRunTableSRP018247.txt | tail --lines=+2 | sort > data/SRP018247.lst
+#cut -f 6 data/SraRunTableSRP018246corrected.txt | tail --lines=+2 | sort > data/SRP018246.lst
+#cut -f 6 data/SraRunTableSRP018247.txt | tail --lines=+2 | sort > data/SRP018247.lst
 
 # download  Trachymyrmex/Cyphomyrmex
 cd data/SRP018246
